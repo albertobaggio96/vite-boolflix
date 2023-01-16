c7a8aa9d61c67842b7c786a1ffbe9aab03021d02
albertobaggio96 <albertobaggio96@gmail.com>
2023-01-16 11:52:21 +0100

    replaced language abbreviation with flag

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -7,17 +7,23 @@ export default {
     return{
       store,
     }
-  }
-  
+  },
+  methods:{
+    getImgPath(imgPath){
+      return new URL(`../assets/img/${imgPath}.png`, import.meta.url).href;
+    }
+  },
 }
 </script>
 
 <template>
   <main>
-    <div v-for="film in store.filmsList">{{film.title}}, {{film.original_title}}, {{film.original_language}}, {{film.vote_average}}</div>
+    <div v-for="film in store.filmsList">{{film.title}}, {{film.original_title}}, <img :src="getImgPath(film.original_language)" :alt="film.original_language">, {{film.vote_average}}</div>
   </main>
 </template>
 
 <style lang="scss">
-  
+  img{
+    width: 30px;
+  }
 </style>
