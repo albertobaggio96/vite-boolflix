a9fc70be6fb99805a6206b7cb7903b6732660c77
albertobaggio96 <albertobaggio96@gmail.com>
2023-01-17 12:57:15 +0100

    add all cast

--- src/components/main-components/CastApp.vue
+++ src/components/main-components/CastApp.vue
@@ -1,22 +1,53 @@
 <script>
-  export default {
-    props:[
-      "id",
-      "apiKey"
-    ],
+import axios from 'axios';
 
-    data(){
-      return{
-        creditsURL: "https://api.themoviedb.org/3/movie/",
-      }
+
+export default {
+  props: [
+    "id",
+    "apiKey"
+  ],
+
+  data() {
+    return {
+      creditsURL: "https://api.themoviedb.org/3/movie/",
+      castList : [],
     }
+  },
+  methods: {
+    getCreditsURLMovie(){
+      return `${this.creditsURL}${this.id}/credits`
+    },
+    getCastInfo() {
+      axios.get(this.getCreditsURLMovie(), {
+        params: {
+          api_key: this.apiKey
+        }
+      })
+        .then((response) =>{
+          // console.log(response.data.cast);
+          this.castList = response.data.cast
+          console.log(this.castList)
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+        .then(function () {
+          // always executed
+        });
+    }
+  },
+  created(){
+    this.getCastInfo()
+    this.getCastInfo()
   }
+}
 
 </script>
 
 <template >
-  <div>
-    {{ apiKey }}
+  <div v-for="cast in castList">
+    {{ cast.name }}
   </div>
 </template>
 
